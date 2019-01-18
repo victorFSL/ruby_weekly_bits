204d4817bd8564bb8ecb8090e4b3319b669a3c8e
Victor Santana <victorsantana@Victors-MacBook-Air.local>
2019-01-18 16:59:19 -0700

    Reverse words in a string

--- Ruby Bites - Code for the week/reverse_words.rb
+++ Ruby Bites - Code for the week/reverse_words.rb
@@ -1,2 +1,18 @@
 #This is NOT an excercise to reverse characters
 # but to reverse words
+require 'rspec'
+
+
+str = "going? it is how everyone, Hello"
+
+def reverse_string string
+  string.split.reverse.join(' ')
+end
+
+
+
+describe 'Reverses string' do
+  it 'reverses the words in a string' do
+    expect(reverse_string(str)).to eq('Hello everyone, how is it going?')
+  end
+end
